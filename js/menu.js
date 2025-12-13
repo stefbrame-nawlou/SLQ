de471b1215c86abf899c4cf158c43801c7b2ec9f
BRAME Stefano <bramest@touring.be>
2025-12-13 20:00:37 +0100

    Initial menu html/css/js/json

---
+++ js/menu.js
@@ -0,0 +1,55 @@
+const menuEl = document.getElementById("menu");
+const startBtn = document.getElementById("startBtn");
+
+async function loadMenu() {
+  const categoriesRes = await fetch("data/categories.json");
+  const categories = await categoriesRes.json();
+
+  categories.forEach(async (cat) => {
+    const catRes = await fetch(`data/${cat.file}`)
+    const catData = await catRes.json();
+
+    const totalWords = catData.words.length;
+    const totalGroups = Math.ceil(totalWords/10);
+
+    const catDiv = document.createElement("div");
+    catDiv.className = "category";
+    
+    //category title
+    const title = document.createElement("div");
+    title.className = "category-title";
+    title.textContent = cat.label
+    catDiv.appendChild(title);
+    
+    for let(i = 0; i < totalGroups; i++){
+      const groupDiv = document.createElement("ipnut");
+      checkbox.type = "checkbox"
+      checkbox.className = "group-checkbox";
+      checkbox.id = `${cat.file}-group-${i}`;
+      checkbox.dataset.categoryFile = cat.file;
+      checkbox.dataset.groupIndex = i;
+
+      const start = i*10 +1;
+      const end = Math.min((i + 1)*10, totalWords);
+
+      const lbl = document.createElement("label");
+      lbl.setAttribute("for", checkbox.id);
+      lbl.textContent = `Words ${start}-${end}`;
+
+      // Optional tooltip (hover to see words)
+      lbl.title = catData.words.slice(i*10, (i+1)*10).map(w => w.word).join(", ");
+
+      groupDiv.appendChild(checkbox);
+      groupDiv.appendChild(label);
+      catDiv.appendChild(groupDiv);
+    }
+
+    menuEl.appendChild(groupDiv);
+  });
+}
+
+// startBtn.querySelectorAll("input.group-checkbox:checked").forEach(cb => {
+//   selectedGroups.push({
+//     categoryFile
+//   })
+// })
